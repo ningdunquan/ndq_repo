165edac5e4fd192baf8840251a45ffeb092fdc12
ndq <ningdunquan@163.com>
2015-11-30 22:55:49 +0800

    "20151130_ndqOne_commit"

--- helloworld/src/com/hello/world/java/Main.java
+++ helloworld/src/com/hello/world/java/Main.java
@@ -3,8 +3,12 @@ package com.hello.world.java;
 public class Main {
 
 	public static void main(String[] args) {
+		Person person = new Person(002, "ningdunquan", 21);
+		
 		System.out.println("Hello world");
 		System.out.println("Hello ShangHai");
 		System.out.println("Hello Ningdunquan");
+		
+		System.out.println(person);
 	}
 }
