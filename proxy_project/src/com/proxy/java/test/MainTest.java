9b1ee5361f61bab067e782be0028c4526d0cf732
ndq <ningdunquan@163.com>
2015-12-02 22:38:03 +0800

    -a

--- proxy_project/src/com/proxy/java/test/MainTest.java
+++ proxy_project/src/com/proxy/java/test/MainTest.java
@@ -1,5 +1,7 @@
 package com.proxy.java.test;
 
+import java.lang.reflect.Method;
+
 import com.proxy.java.business.AddBusiness;
 import com.proxy.java.business.impl.AddBusinessImpl;
 import com.proxy.java.handler.NdqInvocationHandler;
@@ -8,13 +10,28 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		AddBusiness business = new AddBusinessImpl();
+
+		AddBusiness proxyed = (AddBusiness) new NdqInvocationHandler()
+				.getProxyed(business);
+
+		// double sum = proxyed.add(12, 31, -14);
+		//
+		// System.out.println(sum);
+		//
+		// System.out.println(proxyed.add(11.12, 10.22, 11, 223, -100));
+
+		Method[] methods = proxyed.getClass().getMethods();
+
+		for (Method method : methods) {
+			System.out.println(method.getGenericReturnType().getTypeName()
+					+ " " + method.getName());
+		}
 		
-		AddBusiness proxyed = (AddBusiness) new NdqInvocationHandler().getProxyed(business);
+		//Ã»ÓÐ°ü
+//		System.out.println(proxyed.getClass().getPackage().getName());
 		
-		double sum = proxyed.add(12, 31, -14);
-		
-		System.out.println(sum);
-		
-		System.out.println(proxyed.add(11, 10));
+		System.out.println(proxyed.getClass().getName());
+		System.out.println(proxyed.getClass().getSuperclass().getName());
+
 	}
 }
